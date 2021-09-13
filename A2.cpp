92560accdd2b22af7e96aa7791600f8c9db4b829
Miguel Antonio Bastidas Verdin <75141067+ELBASTIDAAS@users.noreply.github.com>
2021-09-13 15:21:59 -0700

    Add files via upload

---
+++ A2.cpp
@@ -0,0 +1,37 @@
+/******************************************************************************
+    Problema 31
+    
+Determinar si un entero dado es divisible entre 2 y 5 
+
+Bastidas Verdin Miguel Antonio
+
+
+*******************************************************************************/
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+main()
+
+{
+   char w='S';
+   int a;
+   cout <<"introduzca un numero para determinar si";
+   cout <<"\n es divisible de 2 , 5 : ";
+   cin >>a;
+   
+      if (a%2 == 0)
+      {
+         
+         cout<<"\t \n el numero " <<a << " es divisible de 2";
+       }
+      if (a%5 == 0)
+      {
+         
+         cout<<"\t \n el numero " <<a << " es divisible de 5";
+       }
+
+    
+}
