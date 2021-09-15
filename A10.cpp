2f0005141164317a5f2d63e139e3f413a805704d
Miguel Antonio Bastidas Verdin <75141067+ELBASTIDAAS@users.noreply.github.com>
2021-09-15 15:14:36 -0700

    Add files via upload

---
+++ A10.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/******************************************************************************
+ Problema 40
+ 
+ Una pulgada equivale a 2.5cm, calcular el numer en cm en 32 pulgadas
+ 
+ Bastidas Verdin Miguel Antonio
+ 
+ @ELBASTIDAAS
+
+*******************************************************************************/
+
+
+int main()
+{
+    double P = 2.5;
+    double x = 0;
+    
+    x = 32 * P;
+    
+    cout << x;
+    
+    return 0;
+}
