b5bc41b318c88ecf377d5f3f2b64f1fb44fe944f
Miguel Antonio Bastidas Verdin <75141067+ELBASTIDAAS@users.noreply.github.com>
2021-09-15 02:12:00 -0700

    Update A4.cpp

--- A4.cpp
+++ A4.cpp
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <math.h>
+
 /******************************************************************************
 
 Problema 34
@@ -5,9 +8,11 @@ Problema 34
 Determinar si la suma 3^1974 + 3^1974 + 3^1974 es igual a 3^1975  
 
 Bastidas Verdin Miguel Antonio
+
+@ELBASTIDAAS
+
 *******************************************************************************/
-#include <stdio.h>
-#include <math.h>
+
 
 int main() 
 {
